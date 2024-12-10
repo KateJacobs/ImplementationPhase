a777496b3fff1118dddfec51c623299762c2b919
KateJacobs <kjacobs@ltu.edu>
2024-12-10 17:44:16 -0500

    implementationFinal.cpp

--- implementationFinal.cpp
+++ implementationFinal.cpp
@@ -551,10 +551,10 @@ public:
 
     // determine risk level (out of 53 points)
     void calculateFinalRisk() {
-        if (totalRisk >= 40) {
+        if (totalRisk >= 36) {
             cout << "Low Risk";
         }
-        else if (totalRisk >= 26) {
+        else if (totalRisk >= 24) {
             cout << "Moderate Risk";
         }
         else {
@@ -582,7 +582,7 @@ private:
     vector<string> mentalHealthBank;
 
     void questionBank() {
-        exerciseBank = { "1. Take short, leisurely walks in a safe and familiar environment for 15–20 minutes daily\n",
+        exerciseBank = { "1. Take short, leisurely walks in a safe and familiar environment for 15Â–20 minutes daily\n",
         "2. Perform seated exercises, such as arm raises, leg lifts, or seated marching.\n",
         "3. Engage in light stretching or gentle yoga to improve flexibility and relaxation.\n",
         "4. Practice breathing exercises combined with light arm movements to enhance lung capacity.\n",
@@ -590,7 +590,7 @@ private:
 
         cognitiveExerciseBank = { "1. Engage in brain-stimulating activities, such as puzzles, chess, or learning a new skill.\n",
         "2. Maintain regular social interactions to support emotional and cognitive health.\n",
-        "3. Continue with good sleep hygiene—quality sleep promotes cognitive resilience.\n",
+        "3. Continue with good sleep hygieneÂ—quality sleep promotes cognitive resilience.\n",
         "4. Eat foods that support brain health, like fatty fish, nuts, and whole grains. \n",
         "5. Stay physically active, as exercise improves blood flow to the brain.\n" };
 
@@ -598,7 +598,7 @@ private:
         "2. Engage in social interactions with friends and family to foster emotional connections.\n",
         "3. Continue regular physical exercise to boost endorphins and improve mood.\n",
         "4. Practice mindfulness or meditation to enhance emotional regulation.\n",
-        "5. Get 7–9 hours of restful sleep to support mental well-being.\n" };
+        "5. Get 7Â–9 hours of restful sleep to support mental well-being.\n" };
 
     }
 
@@ -606,10 +606,10 @@ public:
     void generateRecommendations(double lifestyleScore, double geneticScore, double cognitiveScore) {
         questionBank();
         // physical recommendations
-        if (lifestyleScore >= 15) {
-            cout << "Lifestyle Low Risk Recommendations: Take short, leisurely walks in a safe and familiar environment for 15–20 minutes daily" << endl;
+        if (lifestyleScore >= 36) {
+            cout << "Lifestyle Low Risk Recommendations: Take short, leisurely walks in a safe and familiar environment for 15Â–20 minutes daily" << endl;
         }
-        else if (lifestyleScore >= 8 && lifestyleScore <= 14) {
+        else if (lifestyleScore >= 24) {
             cout << "Lifestyle Moderate Risk Recomendations: Perform standing exercises with support, such as squats while holding onto a stable surface." << endl;
         }
         else {
@@ -617,10 +617,10 @@ public:
         }
 
         // cognitive recommendations
-        if (cognitiveScore >= 15) {
+        if (cognitiveScore >= 36) {
             cout << "Cognitive Low Risk Recommendations: Engage in brain-stimulating activities, such as puzzles, chess, or learning a new skill." << endl;
         }
-        else if (cognitiveScore >= 8 && cognitiveScore <= 14) {
+        else if (cognitiveScore >= 24) {
             cout << "Cognitive Moderate Risk Recomendations: Schedule cognitive assessments or memory screenings to track changes over time." << endl;
         }
         else {
@@ -628,10 +628,10 @@ public:
         }
 
         // mental health recommendations
-        if (lifestyleScore >= 15) {
+        if (lifestyleScore >= 36) {
             cout << "Cognitive Low Risk Recommendations: Engage in social interactions with friends and family to foster emotional connections." << endl;
         }
-        else if (cognitiveScore >= 8 && cognitiveScore <= 14) {
+        else if (cognitiveScore >= 24) {
             cout << "Cognitive Moderate Risk Recomendations: Seek professional counseling or therapy to address emotional concerns." << endl;
         }
         else {
